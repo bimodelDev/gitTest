ba9f2f1d687a70746f17e92a8eeff900aa189aa1
bimodel3 <bimodel3@DESKTOP-03IEUKV>
2023-10-28 17:10:59 +0300

    aaa

---
+++ testFile/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+    Public Class ZeroDivisonError : Inherits Exception
+        Public Sub New()
+            Console.WriteLine("ZeroDivisionError")
+        End Sub
+    End Class
+    Sub Main()
+        Dim numerator As Integer = 15
+        Dim Denominator As Integer = 0
+        If Denominator = 0 Then
+            Throw New ZeroDivisonError()
+        End If
+    End Sub
+End Module
