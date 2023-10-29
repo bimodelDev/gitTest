be59eac692701092be68058b00497763602bc951
bimodel3 <bimodel3@DESKTOP-03IEUKV>
2023-10-29 16:09:28 +0300

    getting random file name

--- testFile/Module1.vb
+++ testFile/Module1.vb
@@ -1,14 +1,8 @@
-﻿Module Module1
-    Public Class ZeroDivisonError : Inherits Exception
-        Public Sub New()
-            Console.WriteLine("ZeroDivisionError")
-        End Sub
-    End Class
+﻿Imports System.IO
+
+Module Module1
     Sub Main()
-        Dim numerator As Integer = 15
-        Dim Denominator As Integer = 0
-        If Denominator = 0 Then
-            Throw New ZeroDivisonError()
-        End If
+        Console.WriteLine(IO.Path.GetFileNameWithoutExtension(IO.Path.GetRandomFileName()))
+
     End Sub
 End Module
